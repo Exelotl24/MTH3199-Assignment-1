76fa2d56a7c6859929bc99fa09dd93d93f8adf40
mirachew <mira@thechewfamily.com>
2025-09-05 01:14:44 -0400

    Updated Newton function and main

--- main.m
+++ main.m
@@ -5,11 +5,18 @@ test_func01 = @(x) (x.^3)/100 - (x.^2)/8 + 2*x + 6*sin(x/2+6) -0.7 - exp(x/6);
 test_derivative01 = @(x) 3*(x.^2)/100 - 2*x/8 + 2 +(6/2)*cos(x/2+6) - exp(x/6)/6;
 
 
-test_func = @(x) x.^2-2;
+% test_func01 = @(x) x.^2-2;
+% test_derivative01 = @(x) 2*x;
+
+functions = {test_func01, test_derivative01};
+
+x = newton_solver(functions, 40);
 
 x_left = -15;
 x_right = 40;
-x = bisection_solver(test_func01, x_left, x_right);
+% x = bisection_solver(test_func01, x_left, x_right);
+
+
 
 figure()
 fplot(test_func01, [x_left, x_right])
--- newton_solver.m
+++ newton_solver.m
@@ -1,11 +1,15 @@
 %Note that fun(x) should output [f,dfdx], where dfdx is the derivative of f
-function x = newton_solver(fun,derfun,xn,tol)
-%Definition of the test function and its derivative
-% test_func01 = @(x) (x.ˆ3)/100 - (x.ˆ2)/8 + 2*x + 6*sin(x/2+6) -.7 - exp(x/6);
-% test_derivative01 = @(x) 3*(x.ˆ2)/100 - 2*x/8 + 2 +(6/2)*cos(x/2+6) - exp(x/6)/6;
-    while abs(fun(xn)) > tol
-    xn = xn - (fun(xn)/derfun(xn));
-    end
+function x = newton_solver(functions, x)
+    
+    % define tolerance and functions
+    tol = 1e-6;
+    func = functions{1};
+    dfdx = functions{2};
 
-disp(['Root','xn', num2str(xn), 'f(x)', num2str(fun(xn))]);
+    % newton's update loop is x_(n+1) = x_n - f(x_n)/f'(x_n)
+    while abs(func(x)) > tol && abs(dfdx(x))>tol
+        x = x - (func(x)/dfdx(x));
+    end
+    
+    % disp(['Root','xn', num2str(xn), 'f(x)', num2str(fun(xn))]);
 end

3100b231d0dd628af782c31bf48d4328804612b8
Exelotl24 <elauer@olin.edu>
2025-09-05 16:14:20 -0400

    Update newton_solver.m

--- newton_solver.m
+++ newton_solver.m
@@ -1,14 +1,16 @@
 %Note that fun(x) should output [f,dfdx], where dfdx is the derivative of f
-function x = newton_solver(functions, x)
+function [x, x_guesses] = newton_solver(functions, x)
     
     % define tolerance and functions
     tol = 1e-6;
     func = functions{1};
     dfdx = functions{2};
+    x_guesses = [];
 
     % newton's update loop is x_(n+1) = x_n - f(x_n)/f'(x_n)
     while abs(func(x)) > tol && abs(dfdx(x))>tol
         x = x - (func(x)/dfdx(x));
+        x_guesses = [x_guesses, x];
     end
     
     % disp(['Root','xn', num2str(xn), 'f(x)', num2str(fun(xn))]);
